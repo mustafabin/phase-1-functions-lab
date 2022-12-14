df8241ecf21656738995dafbf017ffa5df436320
Mustafa Binalhag <mbin828@gmail.com>
2022-12-14 10:46:47 -0500

    math abs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+
+let ft = 264
+let HQ = 42
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - HQ)
+}
+
+
+
+function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+
+
+}
+
+
+console.log(distanceFromHqInBlocks(50))
