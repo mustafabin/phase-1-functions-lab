c479347d91a869b821d33a04cf65c4d1d5c4a373
Mustafa Binalhag <mbin828@gmail.com>
2022-12-14 11:05:43 -0500

    third function

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ let ft = 264
 let HQ = 42
 
 function distanceFromHqInBlocks(someValue) {
-    return Math.abs(someValue - HQ)
+    return Math.abs(someValue - HQ)  
 }
 
 
 
-function distanceFromHqInFeet(someValue) {
-    distanceFromHqInBlocks(someValue);
+function distanceFromHqInFeet(blocks) {
+    let difference = distanceFromHqInBlocks(blocks);
     // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
     // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
   
     // the return value of distanceFromHqInBlocks can then be used to calculate feet
-
+    return difference * ft
 
 }
 
 
-console.log(distanceFromHqInBlocks(50))
+
+
+function distanceTravelledInFeet(start, destination) {
+    let difference = Math.abs(start - destination)
+    return difference * ft
+    //returns the number of feet traveled
+}
+console.log(distanceTravelledInFeet(1,5))
